1f3bce5d10a5af2c46a83fb158a9886e09149b54
NITIN NAUTIYAL <nitinnautiyal2016@gmail.com>
2023-04-26 12:44:23 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import CustomControls from '../components/CustomControls'
 function App() {
   return (
     <>
-      <Canvas camera={{position : [.5,3,4] ,fov : 50}} shadows gl={{preserveDrawingBuffer : true}}>
+      <Canvas camera={{position : [.5,3,4] ,fov : 50}}  shadows gl={{preserveDrawingBuffer : true}}>
       <CustomControls/>
       <ambientLight/>
       <directionalLight castShadow receiveShadow position={[-5, 5, 5]} shadow-mapSize-width={1024} shadow-mapSize-height={1024}/>
